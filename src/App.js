e634b6701856ba98f3434816d13e5c64653c11e3
gerrul3r <shadow_tsw@web.de>
2020-05-20 16:47:03 +0200

    runnable trial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Dapati} from 'dapati.js'
+import Dapati from './dapati.js'
 
 function App() {
   return (
--- src/dapati.js
+++ src/dapati.js
@@ -1,8 +1,21 @@
+import React from 'react';
 
+class Dapati extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            loggedIn: false
+        }
+    }
 
-
-
-
-export class Dapati {
-
+    render() {
+        return(
+            <>
+                <h1>Test</h1>
+                <p>Die ist ein Test</p>
+            </>
+        )
+    }
 }
+
+export default Dapati
