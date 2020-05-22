5b16f99a8c7c1567d79b319fde65bc2564cb187f
gerrul3r <shadow_tsw@web.de>
2020-05-22 10:06:46 +0200

    fetch-API button

--- src/fetchAPI/fetch_sample.js
+++ src/fetchAPI/fetch_sample.js
@@ -1,7 +1,31 @@
 // import React from 'react'
 
-function apiAccess() {
-    return null
+async function apiAccess(url = "", data = {}, token = null, implementMethod = "GET") {
+    let headersContent={};
+
+    if (token !== null) {
+        headersContent = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer${token}`
+        }
+    } else {
+        headersContent = {
+            'Content-Type': 'application/json',
+        }
+    }
+
+    if (data !== {}) {
+        data = JSON.stringify(data)
+    }
+
+    console.log(headersContent)
+
+    const response = await fetch(url, {
+        method: implementMethod,
+        headers: headersContent,
+        body: data
+    });
+    return response.json();
 }
 
 export default apiAccess
--- src/maincontent/maincontent_sample.js
+++ src/maincontent/maincontent_sample.js
@@ -4,12 +4,13 @@ import React from 'react'
 function maincontent(props) {
     return (
         <>
-        <article>
-            <section>
-                <h3 style={{color:"white"}}>Just a header</h3>
-                <p style={{color:"white"}}>Just a Text</p>
-            </section>
-        </article>
+            <article>
+                <section>
+                    <h3 style={{ color: "white" }}>Just a header</h3>
+                    <p style={{ color: "white" }}>Just a Text</p>
+                    <h4>{props.title}</h4>
+                </section>
+            </article>
         </>
     )
 }
